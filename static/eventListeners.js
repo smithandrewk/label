341b88de759153ef7581e4683a8c430cad0ff73a
Andrew <andrewsmith1025@gmail.com>
2025-05-20 21:15:15 -0400

    bout deletion, fa icons

--- static/eventListeners.js
+++ static/eventListeners.js
@@ -1,14 +1,32 @@
 export function addEventListeners() {
     document.addEventListener('keydown', function(event) {
         // For Mac: event.metaKey is Command, for Windows/Linux: event.ctrlKey is Control
-        if ((event.metaKey || event.ctrlKey) && event.key.toLowerCase() === 's') {
-            console.log('Save shortcut triggered');
+        if ((event.metaKey || event.ctrlKey) && (event.key.toLowerCase() === 's') && event.shiftKey) {
             event.preventDefault(); // Prevent browser save dialog
-            // Find the currently visible split button and trigger it
-            const splitButton = document.querySelector('#action-buttons button.btn-warning, #action-buttons button.btn-info');
-            if (splitButton) {
-                splitButton.click();
-            }
+            splitSession();
+        } else if ((event.metaKey || event.ctrlKey) && event.key.toLowerCase() === 's') {
+            event.preventDefault(); // Prevent browser save dialog
+            toggleSplitMode();
+        } else if (event.key.toLowerCase() === 'r' && !(event.metaKey || event.ctrlKey)) {
+            // Check if the 'r' key is pressed without any modifier keys
+            console.log('Creating new bout...');
+            event.preventDefault(); // Prevent browser refresh
+            createNewBout();
         }
     });
+    const backButton = document.getElementById('back-btn-overlay');
+    if (backButton) {
+        backButton.addEventListener('click', function() {
+            const currentUrl = window.location.href;
+            const newUrl = currentUrl.replace(/\/[^\/]*$/, '/');
+            window.history.pushState({ path: newUrl }, '', newUrl);
+            window.location.reload();
+        });
+        backButton.addEventListener('mouseenter', () => {
+            backButton.style.background = 'rgba(0, 0, 0, 0.1)';
+        });
+        backButton.addEventListener('mouseleave', () => {
+            backButton.style.background = 'rgba(0, 0, 0, 0)';
+        });
+    }
 }
