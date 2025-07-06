3757c849b70a37810145a18b86645d711c1973fe
Andrew Smith <andrewsmith1025@gmail.com>
2025-07-06 10:24:58 -0400

    feat: remove gunzip on project upload in favor of standardizing data externally

--- app/services/project_service.py
+++ app/services/project_service.py
@@ -340,35 +340,7 @@ class ProjectService:
                     accel_file = os.path.join(item_path, 'accelerometer_data.csv')
                     if os.path.exists(accel_file):
                         sessions.append({'name': item, 'file': 'accelerometer_data.csv'})
-                    elif os.path.exists(os.path.join(item_path, 'accelerometer_data.gz')):
-                        # Unzip
-                        gz_file = os.path.join(item_path, 'accelerometer_data.gz')
-                        csv_file = os.path.join(item_path, 'accelerometer_data.csv')
-                        # Use sed on macOS to remove last line, head on other systems
-                        import platform
-                        if platform.system() == 'Darwin':  # macOS
-                            os.system(f'gunzip -c "{gz_file}" | sed \'$d\' > "{csv_file}"')
-                        else:
-                            os.system(f'gunzip -c "{gz_file}" | head -n -1 > "{csv_file}"')
-                        sessions.append({'name': item, 'file': 'accelerometer_data.csv'})
-                        os.remove(gz_file)
                     
-                    gyro_data = os.path.join(item_path, 'gyroscope_data.csv')
-                    gz_file = os.path.join(item_path, 'gyroscope_data.gz')
-
-                    if os.path.exists(gyro_data):
-                        logger.debug('Found gyroscope data:', gyro_data)
-                    else:
-                        logger.debug('No gyroscope data found in:', item_path)
-                        if os.path.exists(gz_file):
-                            # Use sed on macOS to remove last line, head on other systems
-                            if platform.system() == 'Darwin':  # macOS
-                                os.system(f'gunzip -c "{gz_file}" | sed \'$d\' > "{gyro_data}"')
-                            else:
-                                os.system(f'gunzip -c "{gz_file}" | head -n -1 > "{gyro_data}"')
-                    if os.path.exists(gz_file):                         
-                        os.remove(gz_file)
-                        
             # Sort sessions by date/time in the name
             try:
                 from datetime import datetime
